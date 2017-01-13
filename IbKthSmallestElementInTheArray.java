12adc33a398fe5a6565814669923cec2484488d7
Javier Arboleda <javier.g.arboleda@gmail.com>
2017-01-12 23:12:26 -0800

    Update IbKthSmallestElementInTheArray.java

--- IbKthSmallestElementInTheArray.java
+++ IbKthSmallestElementInTheArray.java
@@ -7,14 +7,14 @@ import java.util.List;
 
 public class IbKthSmallestElementInTheArray {
 
-    public static int kthsmallest(final List<Integer> a, int k) {
+    public int kthsmallest(final List<Integer> a, int k) {
 
         if (k < 1 || k > a.size())
             throw new ArrayIndexOutOfBoundsException("Input needs to be within array size bounds!");
-
+        
         if (a.isEmpty())
             throw new IllegalArgumentException("Do not pass an empty list!");
-
+        
         Integer[] b = new Integer[a.size()];
 
         b = a.toArray(b);
